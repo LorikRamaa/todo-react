8de00abb1babcd1c6ffc6dcf5872029dc9bc84ad
Lorik Rama <lorik.rama.317@gmail.com>
2024-02-04 14:26:52 +0100

    importing react app

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
+uuidv4();
+
+const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  useEffect(() => {
+    const data = localStorage.getItem("items");
+    // only update the state when the data persists
+    if (data) {
+      setTodos(JSON.parse(data));
+    } else {
+      data = [];
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(todos));
+  }, [todos]);
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <TodoForm addTodo={addTodo} />
+
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoWrapper;
